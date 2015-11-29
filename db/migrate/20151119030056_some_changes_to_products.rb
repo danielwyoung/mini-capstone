b377479f485c9070198eb53b9a47c7ab1c17f73e
dan-young <greenboots@gmail.com>
2015-11-29 10:30:04 -0600

    got rid of precision in decimal statement

--- db/migrate/20151119030056_some_changes_to_products.rb
+++ db/migrate/20151119030056_some_changes_to_products.rb
@@ -1,6 +1,6 @@
 class SomeChangesToProducts < ActiveRecord::Migration
   def change
     change_column :products, :description, :text
-    change_column :products, :price, 'decimal USING CAST(price AS decimal)' precision: 6, scale: 2
+    change_column :products, :price, 'decimal USING CAST(price AS decimal)'
   end
 end
