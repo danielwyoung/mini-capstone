191ab03d7f060c1bd7a72ca1f90aeacd7d61ce09
dan-young <greenboots@gmail.com>
2015-11-29 10:34:26 -0600

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Product.create!([
-  {name: "Dog Collar", price: "3.0", image: "http://www.lovelonglong.com/upload/product/normal/product_1311083896950942568.jpg", description: "Leather Dog Collar", size: "Medium", color: "Red", available: nil},
-  {name: "Cat Collar", price: "4.0", image: "http://ecx.images-amazon.com/images/I/41651tY9EHL.jpg", description: "Jeweled Cat Collar", size: "Small", color: "Pink", available: nil},
-  {name: "Leash", price: "12.0", image: "http://badasspetz.com/images/royal-blue-personalized-leather-dog-leash.jpg", description: "Leather leash", size: "10ft", color: "Blue", available: nil},
-  {name: "Food Bowl", price: "8.0", image: "http://rlv.zcache.com/dog_bowl_by_janz_large_in_sky_blue_pet_bowl-r9a4f4356727c4c6aaa8fb58dfce5b0c9_2iwjt_8byvr_324.jpg", description: "Ceramic Bowl", size: "8 quart", color: "Sky Blue", available: nil},
-  {name: "Chewy Ball", price: "4.0", image: "http://3.bp.blogspot.com/-fAWgDqnVYiw/UEygSgM67uI/AAAAAAAAABI/J-sV_BA8AcA/s300/ChuckIt%2BUltra%2BBalls.jpg", description: "Chewy Ball", size: "N/A", color: "Orange", available: nil}
+  {name: "Dog Collar", price: "3.0", description: "Leather Dog Collar", size: "Medium", color: "Red", available: nil},
+  {name: "Cat Collar", price: "4.0", description: "Jeweled Cat Collar", size: "Small", color: "Pink", available: nil},
+  {name: "Leash", price: "12.0", description: "Leather leash", size: "10ft", color: "Blue", available: nil},
+  {name: "Food Bowl", price: "8.0", description: "Ceramic Bowl", size: "8 quart", color: "Sky Blue", available: nil},
+  {name: "Chewy Ball", price: "4.0", description: "Chewy Ball", size: "N/A", color: "Orange", available: nil}
 ])
