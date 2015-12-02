833efb9b3be797431eabcbe926bb22cabf6bc931
dan-young <greenboots@gmail.com>
2015-12-01 19:12:11 -0600

    added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
-class User < ActiveRecord::Base
+2class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :carted_products
   has_many :orders
   has_many :products
 end
